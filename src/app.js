9ee7cf7f55f9faa3ba1fed11a2f1681d86e26e85
AhmedMuttakin830 <49265511+AhmedMuttakin830@users.noreply.github.com>
2019-04-19 09:08:22 +0600

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
-console.log("app is running");
+// console.log("app is running");
 
 
-// var template = <h1> Travellers Hub</h1>
+// // var template = <h1> Travellers Hub</h1>
 
-var app = { 
-    title : 'Travellers Hub',
-    subtitle : 'Platform for everyone'
-};
+// var app = { 
+//     title : 'Travellers Hub',
+//     subtitle : 'Platform for everyone'
+// };
+
+// var agency = {
+//     name: 'XYZ Agency',
+//     type: 'Land Route',
+//     altType : 'Air Route'
+// };
+
+// var template = (
+//     <div>
+//         <h1>{app.title}</h1>
+//         <p>{app.subtitle}</p>
+//         <h3>{agency.name}</h3>
+//         <p>{agency.type}</p>
+//         <ol>
+//             <li> Deal one </li>
+//             <li> Deal two </li>
+//         </ol> 
+//     </div>
+// );
+
+// var appRoot = document.getElementById('app');
+
+
+
+//   var appRoot = document.getElementById('app');
+
+// ReactDOM.render(template,appRoot);
+
+console.log('Project is running!');
 
 var agency = {
     name: 'XYZ Agency',
@@ -14,23 +43,59 @@ var agency = {
     altType : 'Air Route'
 };
 
-var template = (
+const app = {
+  title: 'TravellersHub',
+  subtitle: 'Platform for everyone',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    render();
+  }
+};
+
+const onRemoveAll = () => {
+  app.options = [];
+  render();
+};
+
+const onMakeDecision = () => {
+  const randomNum = Math.floor(Math.random() * app.options.length);
+  const option = app.options[randomNum];
+  alert(option);
+};
+
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
     <div>
-        <h1>{app.title}</h1>
-        <p>{app.subtitle}</p>
-        <h3>{agency.name}</h3>
-        <p>{agency.type}</p>
-        <ol>
-            <li> Deal one </li>
-            <li> Deal two </li>
-        </ol> 
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{agency.name}</p>
+      <p>{app.options.length > 0 ? 'Here are your deals' : 'No deals'}</p>
+      <button disabled={app.options.length === 0} onClick={onMakeDecision}>Choose better deal?</button>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <ol>
+        {
+          app.options.map((option) => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add Deals</button>
+      </form>
     </div>
-);
+  );
 
-var appRoot = document.getElementById('app');
+  ReactDOM.render(template, appRoot);
+};
 
-
-
-  var appRoot = document.getElementById('app');
-
-ReactDOM.render(template,appRoot);
+render();
